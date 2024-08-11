cb541e93a4f321a0ed0761e6265defb653881c16
AzatSardaryan <azat.sardaryan1@hotmail.com>
2024-08-11 15:44:11 +0100

    Added register functionality

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,21 @@ from flask import request
 
 @app.route("/")
 @app.route("/home")
-
 def home():
     return "Welcome to the Web Monitoring App"
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        phone_number = request.form.get('phone_number')
+        password = request.form.get('password')
+        hashed_password = bycrypt.generate_password_hash(password).decode('utf-8')
+        user = User(username=username, email=email, phone_number=phone_number, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Your account has been created! You can now login.", 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
